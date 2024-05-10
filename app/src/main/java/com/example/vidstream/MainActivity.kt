14f089ac377f0d87de4463cd97759a0f2beec1ec
Satyam Raj <62481122+stym-rj@users.noreply.github.com>
2024-05-10 20:55:20 +0530

    Added Search functionality

--- app/src/main/java/com/example/vidstream/MainActivity.kt
+++ app/src/main/java/com/example/vidstream/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.lifecycleScope
@@ -13,31 +14,30 @@ import com.example.vidstream.databinding.ActivityMainBinding
 import com.example.vidstream.networkUtils.ItemsData
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
-import com.example.vidstream.BuildConfig
+import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
 import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
 
 class MainActivity : AppCompatActivity(), MyItemClickListener {
     lateinit var binding: ActivityMainBinding
     lateinit var adapter: ItemsAdapter
-
+    private lateinit var supabase: SupabaseClient
+    private var listItems: List<ItemsData> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        var list: MutableList<ItemsData> = mutableListOf()
 
-        adapter = ItemsAdapter(list, this, this)
+        adapter = ItemsAdapter(listItems, this, this)
         binding.rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         binding.rv.adapter = adapter
 
 
 
         // supabase initialization
-        val supabase = createSupabaseClient(
+        supabase = createSupabaseClient(
             supabaseUrl = "https://bbvarubfymsvzuckhexz.supabase.co",
             supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJidmFydWJmeW1zdnp1Y2toZXh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMzg2MzMsImV4cCI6MjAzMDcxNDYzM30.3YRvHJYkHb_BuAXk_1cjP_baOlIjCk3iwOgQtNk6OKE"
         ) {
@@ -46,17 +46,67 @@ class MainActivity : AppCompatActivity(), MyItemClickListener {
         }
 
 
-
+        // Fetching data
         lifecycleScope.launch {
-            var items: List<ItemsData> = supabase
+            listItems = supabase
                                             .from("items")
                                             .select()
                                             .decodeList()
 
             Toast.makeText(this@MainActivity, "data fetched!", Toast.LENGTH_LONG).show()
-            Log.d("database", items.toString())
-            adapter.refreshList(items)
+            Log.d("database", listItems.toString())
+            adapter.refreshList(listItems)
         }
+
+
+        // Searching
+        binding.searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                if (query.toString().isNullOrBlank()) {
+                    adapter.refreshList(listItems)
+                    return false
+                }
+
+                lifecycleScope.launch {
+//                    val retrievedData = supabase
+//                        .from("items")
+//                        .select {
+//                            filter {
+////                                textSearch("title || ' ' || description || ' ' || channel", "'${query.toString()}'", TextSearchType.NONE)
+//                                textSearch("search_throughout", "$query", TextSearchType.NONE)
+//                            }
+//                        }
+
+//                    retrievedData.data.map {
+//                        Log.d("supa data", it.toString())
+//                    }
+
+//                    Log.d("new data supa", retrievedData.data)
+//                    retrievedData.data.forEach {
+//                        Log.d("new data", it.toString())
+//                    }
+
+//                    val newList = parseJsonToItemsList(retrievedData.data)
+//                    Log.d("new data list", newList.toString())
+
+//                    Log.d("search query", retrievedData.toString())
+
+                    val newList = listItems.filter {
+                        it.title.contains(query.toString(), ignoreCase = true) ||
+                        it.description.contains(query.toString(), ignoreCase = true) ||
+                        it.channel.contains(query.toString(), ignoreCase = true)
+                    }
+                    adapter.refreshList(newList)
+
+                }
+                return false
+            }
+
+        })
     }
 
     override fun onItemClicked(item: ItemsData) {
--- app/src/main/java/com/example/vidstream/VideoActivity.kt
+++ app/src/main/java/com/example/vidstream/VideoActivity.kt
@@ -1,8 +1,16 @@
 package com.example.vidstream
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import com.example.vidstream.databinding.ActivityVideoBinding
 import com.example.vidstream.networkUtils.ItemsData
@@ -10,6 +18,7 @@ import com.example.vidstream.networkUtils.ItemsData
 class VideoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityVideoBinding
     private lateinit var player: ExoPlayer
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +31,28 @@ class VideoActivity : AppCompatActivity() {
         player = ExoPlayer.Builder(this).build()
         binding.vid.player = player
 
+
         val mediaItem = MediaItem.fromUri(item.video)
         player.setMediaItem(mediaItem)
         player.prepare()
         player.play()
 
+        player.addListener(object: Player.Listener {
+            @UnstableApi
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                if (playbackState == Player.STATE_READY) {
+                    binding.vid.hideController()
+                }
+            }
+        })
+
         binding.tvTitle.text = item.title
         binding.tvChannel.text = item.channel
-        binding.tvDescription.text = item.description
+
+        val spannable = SpannableString("Description : \n${item.description}")
+        spannable.setSpan(StyleSpan(Typeface.BOLD), 0, 13, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.tvDescription.text = spannable
     }
 
     override fun onDestroy() {
